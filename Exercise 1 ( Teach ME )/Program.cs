074345722e94c238ca6b706e056bccde382286cc
nanji <nanji@DESKTOP-4A8MIDK>
2022-11-03 20:59:01 +0300

    Add project files.

---
+++ Exercise 1 ( Teach ME )/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_1___Teach_ME__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double side = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            // A * H /2
+            double area = side * h / 2;
+
+
+            Console.WriteLine($"Area: {area:f2}");
+
+
+        }
+    }
+}
